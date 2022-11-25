27760b41636ae32602dbc72b2aa1b86faedae6ed
Arun Bohra <arunbohra12@gmail.com>
2022-11-26 02:27:11 +0530

    Create shows controller, model and routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
+
+import showsRouter from './routes/showsRoutes.js';
 
 const app = express();
 
-// app.use()
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('combined'));
+}
+
+app.use(express.json());
+
+app.use('/api/shows', showsRouter);
 
 export default app;
