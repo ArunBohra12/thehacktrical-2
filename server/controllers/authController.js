c65cecb2e633f5c83bb8d744a48123d75ba35fd3
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-25 23:49:13 +0530

    Handler Func for Authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,58 @@
-import { Jwt } from "jsonwebtoken";
-import { promisify } from "util";
-import crypto from 'crypto'
-import catchAsync from "./catchAsync";
+import { jwt } from 'jsonwebtoken';
+import catchAsync from './catchAsync';
+import User from '../models/userModel';
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + 172800000),
+    httpOnly: true,
+  };
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    user,
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  createSendToken(newUser, 201, res);
+});
+
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    // 1) Check if email and password exist
+    if (!email || !password) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Please provide email and password'
+      })
+    }
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select("+password");
+  
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      res.status(401).json({
+        status: 'error',
+        message: 'Incorrect email and password'
+      })
+    }
+  
+    createSendToken(user, 200, res);
+  });
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,1 +1,7 @@
-const express = require('express');
+import { express } from 'express'
+import authController from '../controllers/authController'
+
+const router = express.Router();
+
+
+module.exports = router;
