1164eb011eed1c6ad88b760cf8faa235d6cad697
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-25 23:28:44 +0530

    Created User and Org Models

---
+++ server/controllers/authController.js
@@ -0,0 +1,4 @@
+import { Jwt } from "jsonwebtoken";
+import { promisify } from "util";
+import crypto from 'crypto'
+import catchAsync from "./catchAsync";
---
+++ server/models/orgModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const orgSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [2, 'A tour name must have more or equal then 10 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: {
+    type: String,
+    default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
+  },
+  orgVision: {
+    type: String,
+    default: '',
+  },
+});
+
+const Organisation = mongoose.model('Organisation', orgSchema);
+
+module.exports = User;
