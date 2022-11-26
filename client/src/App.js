5b5e2c7de650a7e04b9fb974e554490884897383
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-26 11:35:49 +0530

    Created the more-info org page LOL

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Singup from './pages/Singup';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import TellUsMore from './pages/TellUsMore';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/register' element={<Singup/>} />
           <Route path='/login' element={<Login/>} />
+          <Route path='/moreinfo' element={<TellUsMore/>} />
           <Route path='/' element={<Home/>} />
         </Routes>
       </BrowserRouter>
