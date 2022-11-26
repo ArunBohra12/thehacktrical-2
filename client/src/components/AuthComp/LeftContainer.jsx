749c11d1cc77bc58c1d5dd6a592bb5d5d74ab66b
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-26 10:48:51 +0530

    Finishing Styling of Signup Page

---
+++ client/src/components/AuthComp/LeftContainer.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  position: relative;
+  height: calc(100vh - 40px);
+  width: calc(40vw - 20px);
+  background: #0a1e8f;
+  border-radius: 20px;
+  color: 1px solid white;
+
+  h1,
+  h4 {
+    color: #ffffff;
+  }
+
+  h1 {
+    text-align: left;
+    width: 60%;
+    margin-left: 2rem;
+    font-family: 'Urbanist';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 46px;
+    color: #ffffff;
+    margin-bottom: 1rem;
+    /* border: 1px solid red; */
+  }
+
+  h4 {
+    text-align: left;
+    margin-left: 2rem;
+    width: 80%;
+    font-family: 'Urbanist';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 38px;
+    color: #ffffff;
+    /* border: 1px solid red; */
+  }
+`;
+
+const Logo = styled.div`
+  position: absolute;
+  top: 3.5rem;
+  left: 2rem;
+
+  h3 {
+    font-family: 'Urbanist', sans-serif;
+    font-style: normal;
+    color: #ffffff;
+    font-weight: 700;
+    font-size: 22px;
+  }
+`;
+
+const LeftContainer = ({ heading, subHeading }) => {
+  return (
+    <Section>
+      <Logo>
+        <h3>theatrify</h3>
+      </Logo>
+      <h1>{heading}</h1>
+      <h4>{subHeading}</h4>
+    </Section>
+  );
+};
+
+export default LeftContainer;
--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -1,114 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import LeftContainer from '../components/AuthComp/LeftContainer';
 
 const Section = styled.div`
   width: 100vw;
   padding: 20px;
   display: flex;
-  background: #1e1e1e;
-`;
-
-const LeftContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  position: relative;
-  height: calc(100vh - 40px);
-  width: calc(40vw - 20px);
-  background: #0a1e8f;
-  border-radius: 20px;
-  color: 1px solid white;
-
-  h1,
-  h4 {
-    color: #ffffff;
-  }
-
-  h1 {
-    text-align: left;
-    width: 60%;
-    margin-left: 2rem;
-    font-family: 'Urbanist';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 46px;
-    color: #ffffff;
-    margin-bottom: 1rem;
-    /* border: 1px solid red; */
-  }
-
-  h4 {
-    text-align: left;
-    margin-left: 2rem;
-    width: 80%;
-    font-family: 'Urbanist';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 25px;
-    line-height: 38px;
-    color: #ffffff;
-    /* border: 1px solid red; */
-  }
-`;
-
-const Logo = styled.div`
-  position: absolute;
-  top: 3.5rem;
-  left: 2rem;
-
-  h3 {
-    font-family: 'Urbanist', sans-serif;
-    font-style: normal;
-    color: #ffffff;
-    font-weight: 700;
-    font-size: 22px;
-  }
+  background: #070a0c;
 `;
 
 const RightContainer = styled.div`
-  border: 1px solid red;
+  /* border: 1px solid red; */
   width: calc(60vw - 20px);
   color: #ffffff;
 `;
 
-const Heading = styled.div`
-  /* border: 1px solid red; */
-  text-align: left;
-  margin-left: 3rem;
-  margin-top: 2rem;
-  h2,
-  h4 {
-    font-family: 'Urbanist';
-    font-style: normal;
-    color: #ffffff;
-  }
+// const Heading = styled.div`
+//   /* border: 1px solid red; */
+//   text-align: left;
+//   margin-left: 3rem;
+//   margin-top: 2rem;
+//   h2,
+//   h4 {
+//     font-family: 'Urbanist';
+//     font-style: normal;
+//     color: #ffffff;
+//   }
 
-  h2 {
-    font-family: 'Urbanist';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 34px;
-    line-height: 53px;
-    color: #ffffff;
-    margin-bottom: 0.8rem;
-  }
+//   h2 {
+//     font-family: 'Urbanist';
+//     font-style: normal;
+//     font-weight: 700;
+//     font-size: 34px;
+//     line-height: 53px;
+//     color: #ffffff;
+//     margin-bottom: 0.8rem;
+//   }
 
-  h4 {
-    font-weight: 500;
-    font-size: 22px;
+//   h4 {
+//     font-weight: 500;
+//     font-size: 22px;
 
-    a {
-      text-decoration: underline;
-    }
-  }
-`;
+//     a {
+//       text-decoration: underline;
+//     }
+//   }
+// `;
 
 const RegisterForm = styled.div`
-  border: 1px solid red;
-  margin-left: 3rem;
-  margin-top: 2rem;
+  /* border: 1px solid red; */
+  margin-left: 4rem;
+  margin-top: 1rem;
   display: flex;
 
   form {
@@ -117,26 +60,111 @@ const RegisterForm = styled.div`
     align-items: flex-start;
     justify-content: center;
 
-    label{
-        font-family: 'Urbanist';
-font-style: normal;
-font-weight: 600;
-font-size: 22px;
-/* line-height: 31px; */
-color: #FFFFFF;
+    label.upperInputs {
+      font-family: 'Urbanist';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 22px;
+      /* line-height: 31px; */
+      color: #ffffff;
     }
 
-    input{
-        display: flex;
-flex-direction: row;
-align-items: flex-start;
-padding: 12px 16px;
-width: 664px;
-background: #151A1E;
-border-radius: 8px;
+    input.grey-inputs {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      padding: 12px 16px;
+      width: 45vw;
+      margin: 0.5rem 0 1rem 0;
+      background: #151a1e;
+      border-radius: 8px;
+      color: #3e4245;
+      font-family: 'Urbanist';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 20px;
+      &::placeholder {
+        font-family: 'Urbanist';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        color: #3e4245;
+      }
+
+      &:focus{
+        outline: none;
+      }
     }
   }
 `;
+
+const RadioButtons = styled.div`
+  display: flex;
+  margin: 1rem 0 1.4rem;
+`;
+
+const RadioButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px 24px;
+  isolation: isolate;
+
+  width: 260px;
+  margin-right: 1rem;
+  height: 63px;
+
+  background: #151a1e;
+  border-radius: 8px;
+
+  label {
+    font-family: 'Urbanist';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 22px;
+    color: #d0d5dd;
+  }
+
+  input {
+    width: 2rem;
+    cursor: pointer;
+  }
+`;
+
+const Buttons = styled.div`
+display: flex;
+/* align-items: center; */
+/* justify-content: space-between; */
+/* border: 1px solid red; */
+width: 40vw;
+
+button{
+  padding: 12px 40px;
+      background: #0a1e8f;
+      border-radius: 8px;
+      font-family: 'Urbanist';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 22px;
+      /* line-height: 31px; */
+      color: #ffffff;
+      cursor: pointer;
+}
+
+div{
+  padding: 12px 40px;
+      background: #0a1e8f;
+      border-radius: 8px;
+      font-family: 'Urbanist';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 22px;
+      /* line-height: 31px; */
+      color: #ffffff;
+      cursor: pointer;
+      margin-left: 2rem;
+}
+`
 
 const Singup = () => {
   const signupHandler = e => {
@@ -145,34 +173,47 @@ const Singup = () => {
   };
   return (
     <Section>
-      <LeftContainer>
-        <Logo>
-          <h3>theatrify</h3>
-        </Logo>
-        <h1>Get started with us!</h1>
-        <h4>A platform where you can buy and sell tickest for local or online theatre events.</h4>
-      </LeftContainer>
+      <LeftContainer
+        heading='Get started with us!'
+        subHeading='A platform where you can buy and sell tickest for local or online theatre events.'
+      />
       <RightContainer>
-        <Heading>
-          <h2>Sign Up</h2>
-          <h4>
-            Already have an account? <Link to='/login'>Log In</Link>
-          </h4>
-        </Heading>
         <RegisterForm>
           <form onSubmit={signupHandler}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id='' />
-            <label htmlFor='name'>Name</label>
-            <input type='text' name='name' id='' />
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' id='' />
-            <label htmlFor='confirmPassword'>Confirm Password</label>
-            <input type='password' name='confirmPassword' id='' />
-            <label htmlFor=''>Who are you?</label>
-            <input type='radio' name='' id='' />
-            <input type='radio' name='' id='' />
+            <label htmlFor='email' className='upperInputs'>
+              Email
+            </label>
+            <input type='email' name='email' placeholder='example@gmail.com' className='grey-inputs' />
+            <label htmlFor='name' className='upperInputs'>
+              Name
+            </label>
+            <input type='text' name='name' placeholder='Adarsh Dubey' className='grey-inputs' />
+            <label htmlFor='password' className='upperInputs'>
+              Password
+            </label>
+            <input type='password' name='password' placeholder='*******' className='grey-inputs' />
+            <label htmlFor='confirmPassword' className='upperInputs'>
+              Confirm Password
+            </label>
+            <input type='password' name='confirmPassword' placeholder='*******' className='grey-inputs' />
+            <RadioButtons>
+              <RadioButton>
+                <input type='radio' name='audience' />
+                <label htmlFor='audience'>Audience</label>
+              </RadioButton>
+              <RadioButton>
+                <input type='radio' name='organisation' />
+                <label htmlFor='organisation'>Organisation</label>
+              </RadioButton>
+            </RadioButtons>
+            <Buttons>
             <button type='submit'>Submit</button>
+            <Link to="/login" >
+            <div>
+            Sign In
+            </div>
+            </Link>
+            </Buttons>
           </form>
         </RegisterForm>
       </RightContainer>
