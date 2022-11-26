de0e02c058411bb50d5126323ba749ae631c5b05
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-26 11:38:01 +0530

    Moving BrowserRouter to Index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Singup from './pages/Singup';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,14 +8,12 @@ import TellUsMore from './pages/TellUsMore';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Routes>
           <Route path='/register' element={<Singup/>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/moreinfo' element={<TellUsMore/>} />
           <Route path='/' element={<Home/>} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
