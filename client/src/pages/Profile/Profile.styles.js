c1bb505df3fc52c4bdc649a6fafbf2e149d2d678
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-27 16:42:05 +0530

    Integrating Profile API

--- client/src/pages/Profile/Profile.styles.js
+++ client/src/pages/Profile/Profile.styles.js
@@ -4,40 +4,40 @@ export const Section = styled.div`
   width: 100vw;
   height: 50vh;
   position: relative;
-  background-color: #070B0D;
-  `;
+  background-color: #070b0d;
+`;
 
 export const OrgButtons = styled.div`
-width: 80%;
-margin: 32px auto;
+  width: 80%;
+  margin: 32px auto;
   display: flex;
   gap: 32px;
 
   a {
-      padding: 16px 24px;
-      color: #fff;
-      background-color: #283BA7;
-      border-radius: 4px;
-      font-family: Urbanist, sans-serif;
-      font-size: 16px;
-      font-weight: 500;
+    padding: 16px 24px;
+    color: #fff;
+    background-color: #283ba7;
+    border-radius: 4px;
+    font-family: Urbanist, sans-serif;
+    font-size: 16px;
+    font-weight: 500;
 
-      &:hover {
-        cursor: pointer;
-      }
+    &:hover {
+      cursor: pointer;
     }
-    `;
+  }
+`;
 
 export const Header = styled.div`
-background-color: #070B0D;
-text-align: left;
-font-family: Urbanist, sans-serif;
-color: #fff;
+  background-color: #070b0d;
+  text-align: left;
+  font-family: Urbanist, sans-serif;
+  color: #fff;
 
-.header {
-      width: 80%;
-      max-width: 1200px;
-      margin: 64px auto 0 auto;
+  .header {
+    width: 80%;
+    max-width: 1200px;
+    margin: 64px auto 0 auto;
     display: flex;
     gap: 64px;
   }
@@ -88,5 +88,60 @@ color: #fff;
       font-size: 28px;
       font-weight: 700;
     }
+  }
+`;
+
+export const Videos = styled.div`
+  /* border: 1px solid white; */
+  width: 90%;
+  margin: 0 auto;
+  text-align: left;
+  margin-top: 3rem;
+
+  h2 {
+    font-family: 'Urbanist';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 46px;
+    color: #ffffff;
+  }
+`;
+
+export const VideoGrid = styled.div`
+display: grid;
+margin-top: 2rem;
+grid-template-columns: auto auto auto;
+/* border: 2px solid white; */
+`
+
+export const Video = styled.div`
+/* border: 2px solid white; */
+background-color: grey;
+width: 26vw;
+margin: 0 auto;
+background: #D9D9D9;
+border-radius: 8px;
+cursor: pointer;
+
+h4{
+  /* border: 2px solid white; */
+  width: 100%;
+  text-align: center;
+  /* margin: 0 auto; */
+  margin-top: 0.8rem;
+  margin-bottom: 0.8rem;
+  font-family: 'Urbanist';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    /* color: #ffffff; */
+}
+
+img {
+  margin: 0 auto;
+    /* border: 2px solid blue; */
+    width: 100%;
+    /* height: 490px; */
+    object-fit: contain;
   }
 `;
--- server/controllers/organisationController.js
+++ server/controllers/organisationController.js
@@ -1,6 +1,7 @@
 import Organisation from '../models/orgModel.js';
 import catchAsync from './catchAsync.js';
 import { multerImageUpload } from '../utils/multer.js';
+import User from '../models/userModel.js';
 
 export const uploadOrgImage = multerImageUpload('public/uploads/images', 'org-img').single('photo');
 
@@ -22,3 +23,21 @@ export const updateOrgInfo = catchAsync(async (req, res) => {
     data: orgData,
   });
 });
+
+
+export const getUser = catchAsync(async (req,res) => {
+  const id = req.params.id;
+
+  const user = await User.findById(id).populate('accessedVideos')
+
+  if (!user) {
+    user = await Organisation.findById(id)
+  }
+
+  // user.populate('accessedVideos', '');
+
+  res.json({
+    status: 'success',
+    user
+  })
+})
