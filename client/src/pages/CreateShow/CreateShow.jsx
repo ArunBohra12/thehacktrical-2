269dc8886011e1a2554ce3adbf2e7a17108c927f
Adarsh Dubey <dubeyadarshmain@gmail.com>
2022-11-27 13:05:49 +0530

    form page design done

--- client/src/pages/CreateShow/CreateShow.jsx
+++ client/src/pages/CreateShow/CreateShow.jsx
@@ -46,13 +46,20 @@ const CreateShow = () => {
       <Navbar />
       <Section>
         <form onSubmit={submitHandler}>
-          <input type='text' name='name' id='' value={showDetails.name} onChange={inputHandler} />
-          <input type="file" ref={thumbnailInput} id="" />
-          <input type='text' name='location' id='' value={showDetails.location} onChange={inputHandler} />
-          <input type='date' name='date' id='' value={showDetails.date} onChange={inputHandler} />
-          <textarea name='description' id='' cols='30' rows='10' value={showDetails.description} onChange={inputHandler} />
-          <input type='number' name='price' id='' value={showDetails.price} onChange={inputHandler} />
-          <button type='submit'>Create Show</button>
+          <label htmlFor="name" className="label">Event Name</label>
+          <input type='text' name='name' id='name' value={showDetails.name} onChange={inputHandler} placeholder="A cool name for your event" />
+          <label htmlFor="thumbnail" className="label">Event Thumbnail</label>
+          <div className="thumbnail-btn">
+          <input type="file" ref={thumbnailInput} id="thumbnail" />Choose a file</div>
+          <label htmlFor="location" className="label">Event Location</label>
+          <input type='text' name='location' id='location' value={showDetails.location} onChange={inputHandler} />
+          <label htmlFor="date" className="label">Event Date</label>
+          <input type='date' name='date' id='date' value={showDetails.date} onChange={inputHandler} />
+          <label htmlFor="description" className="label">Event Description</label>
+          <textarea name='description' id='description' cols='30' rows='10' value={showDetails.description} onChange={inputHandler} placeholder="A brief description that includes everything your audience needs to know."/>
+          <label htmlFor="price" className="label">Event Price (in credits)</label>
+          <input type='number' name='price' id='price' value={showDetails.price} onChange={inputHandler} defaultValue="50" />
+          <button type='submit' className='main-btn'>Create Show</button>
         </form>
       </Section>
     </>
